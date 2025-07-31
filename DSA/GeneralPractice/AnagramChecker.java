32c9ab5095634b40d61fee23c19e9c9026a36766
Abhinav Sharma <0011abhinav@gmail.com>
2025-07-31 17:00:31 +0530

    Today is day 05 of my daily learnings. In today's DSA practice I learnt how to use java's inbuilt sort function to check if two strings are anagrams or not, though it is a bit slower method then my initially method of making a character array and traversing through, it gives me access to check for any string containing either of upper case or lower case letters, I also learnt few map operations by solving for no. of queries questions and finally ended my today's practice with a treemap question where we had to find the frequency of each element in an array and print it in a sorted manner.

---
+++ DSA/GeneralPractice/AnagramChecker.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        
+        scanner.close();
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
