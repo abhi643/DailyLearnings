6a26cdd5ae16b32a04206fcf63100e52194e8996
Abhinav Sharma <0011abhinav@gmail.com>
2025-08-08 19:43:00 +0530

    On day 10 of daily learnings. On my daily quests of learning backend development using SpringBoot, today I implemeted various http mapings such as get/post/delete/patch/put, where i implemented service and dto layer's in order to inject, update and delete data from the database using curl for now because I don't havefrontend as of now, which i'll beadding later.

--- SpringBootBasics/LearningRestAPIs/src/main/java/com/example/LearningRestAPIs/controller/StudentController.java
+++ SpringBootBasics/LearningRestAPIs/src/main/java/com/example/LearningRestAPIs/controller/StudentController.java
@@ -1,26 +1,53 @@
 package com.example.LearningRestAPIs.controller;
 
+import com.example.LearningRestAPIs.dto.AddStudentRequestDto;
 import com.example.LearningRestAPIs.dto.StudentDto;
 import com.example.LearningRestAPIs.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/students")
 public class StudentController {
 
     private final StudentService studentService;
 
-    @GetMapping("/student")
-    public List<StudentDto> getStudent() {
-        return studentService.getAllStudents();
+    @GetMapping
+    public ResponseEntity<List<StudentDto>> getStudent() {
+//        return ResponseEntity.status(HttpStatus.OK).body(studentService.getAllStudents());
+        return ResponseEntity.ok(studentService.getAllStudents());
     }
 
-    @GetMapping("/student/{id}")
-    public StudentDto getStudentById() {
-        return new StudentDto(1, "John", "Doe");
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentDto> getStudentById(@PathVariable long id) {
+//        return ResponseEntity.status(HttpStatus.OK).body(studentService.getStudentById(id));
+        return ResponseEntity.ok(studentService.getStudentById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<StudentDto> createStudent(@RequestBody AddStudentRequestDto addStudentRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentService.addStudent(addStudentRequestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable long id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentDto> updateStudent(@PathVariable long id, @RequestBody AddStudentRequestDto addStudentRequestDto) {
+        return ResponseEntity.ok(studentService.updateStudent(id, addStudentRequestDto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<StudentDto> updateStudentPartially(@PathVariable long id, @RequestBody Map<String, Object> update) {
+        return ResponseEntity.ok(studentService.updateStudentPartially(id, update));
     }
 }
---
+++ SpringBootBasics/LearningRestAPIs/src/main/java/com/example/LearningRestAPIs/dto/AddStudentRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.LearningRestAPIs.dto;
+
+import lombok.Data;
+
+@Data
+public class AddStudentRequestDto {
+    private String name;
+    private String email;
+}
